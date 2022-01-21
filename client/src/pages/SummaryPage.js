6aed7f45edc3dab1f910e7a3e713a95c0e1496c5
greg <grzegorz.ledwon@yahoo.com>
2022-01-21 16:04:45 +0100

    added html for summary page

--- client/src/pages/SummaryPage.js
+++ client/src/pages/SummaryPage.js
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const SummaryPage = () => {
-  return <div>Hi from SummaryPage</div>;
+  return (
+    <div className="card active">
+      <h2>The Minimalist Entrepreneur</h2>
+      <div className="card-header">
+        <div className="total-rating"></div>
+        <Link to="/new" className="btn">
+          Add review
+        </Link>
+      </div>
+      <div className="reviews">
+        <h3>Reviews</h3>
+      </div>
+    </div>
+  );
 };
 
 export default SummaryPage;
