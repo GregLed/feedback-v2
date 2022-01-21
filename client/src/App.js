0b332a0b66ea20d0f3155454263c3677bded0d7e
greg <grzegorz.ledwon@yahoo.com>
2022-01-21 16:06:18 +0100

    added feedback context

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NewFeedbackPage from "./pages/NewFeedbackPage";
 import SummaryPage from "./pages/SummaryPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SummaryPage />} />
-        <Route path="/new" element={<NewFeedbackPage />} />
-      </Routes>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SummaryPage />} />
+          <Route path="/new" element={<NewFeedbackPage />} />
+        </Routes>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
---
+++ client/src/context/FeedbackContext.js
@@ -0,0 +1,54 @@
+import { createContext, useState, useEffect } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/api/reviews`);
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
+  // Add feedback
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/api/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
+  };
+
+  // TODO:
+  // Delete feedback
+  // Update feedback
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        isLoading,
+        addFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
